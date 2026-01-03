52fad115b179adcf7cb039dd7dce77cc3e3d1842
HYEPR <110058070+hyepr@users.noreply.github.com>
2026-01-03 22:07:43 +0000

    Initialized Polar for Better Auth!

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from "better-auth/react";
 import { usernameClient, organizationClient } from "better-auth/client/plugins";
+import { polarClient } from "@polar-sh/better-auth/client";
 
 export const authClient = createAuthClient({
   plugins: [
     usernameClient(),
-    organizationClient()
+    organizationClient(),
+    polarClient()
   ],
 })
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,13 @@
 import { betterAuth } from "better-auth";
 import { username, organization } from "better-auth/plugins";
 import { Pool } from "pg";
+import { polar, checkout, portal, usage } from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
+
+const polarClient = new Polar({
+  accessToken: process.env.POLAR_ACCESS_TOKEN,
+  server: 'sandbox'
+});
 
 export const auth = betterAuth({
   database: new Pool({
@@ -8,7 +15,25 @@ export const auth = betterAuth({
   }),
   plugins: [
     username(),
-    organization()
+    organization(),
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "3ec54eb9-420c-433f-9500-bf36e2f65f4a",
+              slug: "pro"
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true
+        }),
+        portal(),
+        usage()
+      ],
+    })
   ],
   emailAndPassword: {
     enabled: true,
