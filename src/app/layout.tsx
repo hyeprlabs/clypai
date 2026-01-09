30f712eb1d2feafdac9820a9ee18650bba9d9105
HYEPR <110058070+hyepr@users.noreply.github.com>
2026-01-09 16:25:31 +0000

    Added Vercel Speed Insights!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Statsig } from "@/lib/statsig";
 
 const geistSans = Geist({
@@ -52,6 +53,7 @@ export default function RootLayout({
           </Statsig>
           <Toaster />
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
