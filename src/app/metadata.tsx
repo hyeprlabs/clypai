783ed837cc1c6a59701ea72a3223f85d826dfe3e
Oskar-Maria Seeberger <110058070+hyepr@users.noreply.github.com>
2026-02-08 11:42:39 +0000

    feat: refactor layout and metadata handling; integrate providers and system banner

---
+++ src/app/metadata.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  applicationName: "ClypAI",
+  title: {
+    template: "%s | ClypAI",
+    default: "ClypAI",
+  },
+  description: "#1 AI video clipping & editing tool.",
+  keywords: [
+    "AI",
+    "video editing",
+    "video clipping",
+    "content creation",
+    "automation",
+    "ClypAI",
+    "Clip AI",
+  ],
+  creator: "ClypAI Team",
+  publisher: "Hyepr Labs UG",
+  openGraph: {
+    type: "website",
+    url: "https://clypai.com",
+    title: "ClypAI",
+    description: "#1 AI video clipping & editing tool.",
+    siteName: "ClypAI",
+  },
+};
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,29 +2,56 @@
 
 import { ReactNode } from "react";
 
-import { AuthUIProvider } from "@daveyplate/better-auth-ui";
-
 import Link from "next/link";
 
 import { useRouter } from "next/navigation";
 
 import { authClient } from "@/lib/auth-client";
 
+// Providers
+import { ThemeProvider } from "@/components/theme-provider";
+import { AuthUIProvider } from "@daveyplate/better-auth-ui";
+import { SystemBanner } from "@/components/ui/system-banner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export function Providers({ children }: { children: ReactNode }) {
   const router = useRouter()
 
   return (
-    <AuthUIProvider
-      authClient={authClient}
-      navigate={router.push}
-      replace={router.replace}
-      onSessionChange={() => {
-        // Clear router cache (protected routes)
-        router.refresh()
-      }}
-      Link={Link}
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
     >
-      {children}
-    </AuthUIProvider>
+      <AuthUIProvider
+        authClient={authClient}
+        navigate={router.push}
+        replace={router.replace}
+        onSessionChange={() => {
+          // Clear router cache (protected routes)
+          router.refresh()
+        }}
+        Link={Link}
+      >
+        <SystemBanner
+          text="Development Mode"
+          color="bg-orange-500"
+          size="sm"
+          show={false}
+        />
+        <NuqsAdapter>
+          {children}
+        </NuqsAdapter>
+        <Toaster
+          position="bottom-center"
+        />
+        <Analytics />
+        <SpeedInsights />
+      </AuthUIProvider>
+    </ThemeProvider>
   );
 }
