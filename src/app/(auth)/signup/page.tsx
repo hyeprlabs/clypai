e06bbdbb50da8804a5139e7b2b9a45a210ced82f
Oskar-Maria Seeberger <110058070+hyepr@users.noreply.github.com>
2026-02-08 21:53:36 +0000

    feat: simplify login and signup pages; add template component for consistent layout

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
 
-import Link from "next/link";
-
-import { CircleArrowLeft } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-
 import { SignupForm } from "@/components/auth/signup-form";
 
 export const metadata: Metadata = {
@@ -14,26 +8,5 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return (
-    <div
-      className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
-      style={{
-        backgroundImage: "url(/background-auth.jpg)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="absolute top-6 left-6">
-        <Button asChild variant="outline" size="sm" className="rounded-full">
-          <Link href="/">
-            <CircleArrowLeft className="mr-2" />
-            <span>Home</span>
-          </Link>
-        </Button>
-      </div>
-      <div className="w-full max-w-sm">
-        <SignupForm />
-      </div>
-    </div>
-  );
+  return <SignupForm />;
 }
